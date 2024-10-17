928b2ab55e2ad9f05e52c171c1bd123ad4d35fbf
Juniper Watson <lookitsartemis@gmail.com>
2024-10-17 11:22:02 -0500

    Edit main.py

--- src/main.py
+++ src/main.py
@@ -7,14 +7,20 @@ def main():
             text = input("Numbra> ")
             if text.strip() == "":
                 continue
+            
             lexer = Lexer(text)
             parser = Parser(lexer)
-            
-            if lexer.get_next_token()[0] == 'ID' and parser.current_token[0] == 'EQUALS':
-                result = parser.assign()
+
+            if parser.current_token[0] == 'ID':
+                next_token = lexer.get_next_token()
+                if next_token[0] == 'EQUALS':
+                    parser.current_token = next_token
+                    result = parser.assign()
+                else:
+                    result = parser.expr()
             else:
                 result = parser.expr()
-                
+
             print(result)
         except Exception as e:
             print(e)
